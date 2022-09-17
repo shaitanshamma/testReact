de9a5775bd5a3c1180a0f207c11ffd0fd0469cf1
shamma <shammacode@gmail.com>
2022-09-17 21:09:30 +0300

    feat: add useState hooks and inputs handlers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import './App.css';
-import Message from "./components/Message";
+import {useEffect, useState} from "react";
+
 
 function App() {
-    let name = 'Vasya';
-    let lastName = 'Ivanov';
+
+    const [messageList, setMessageList] = useState([]);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+    const [robot, setRobot] = useState('');
+
+    const nameHandler = (e) => {
+        setAuthor(e.target.value)
+    }
+    const textHandler = (e) => {
+        setText(e.target.value)
+    }
+    const messageHandler = () => {
+        setMessageList(messageList => [...messageList, {
+            author: author,
+            text: text
+        }])
+        setAuthor('');
+        setText('');
+    }
+    useEffect(() => {
+        setTimeout(() => {
+            if (messageList.length) {
+                setRobot(messageList[messageList.length - 1].author + ' send some text')
+            }
+        }, 2000)
+    }, [messageList])
+
     return (
         <div className="App">
-            <Message name={name} lastName = {lastName}/>
+            <input type="text" name="" id="" value={author} onChange={nameHandler}/>
+            <input type="text" name="" id="" value={text} onChange={textHandler}/>
+            <button onClick={messageHandler}>Вжух!</button>
+            <ul>{messageList.map((message, idx) =>
+                <li key={idx}>Автор - {message.author} <span>Текст - {message.text}</span></li>)}
+            </ul>
+            <h4>{robot}</h4>
         </div>
     );
 }
